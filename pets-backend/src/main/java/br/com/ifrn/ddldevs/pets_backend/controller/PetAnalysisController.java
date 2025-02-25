65bf43b0d584dddd7adf8930304aed8f6ffb05a2
Lucas <lucasoliveirars22@gmail.com>
2025-02-25 17:16:17 -0300

    :construction: updating controllers to accept multipart image

--- pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/controller/PetAnalysisController.java
+++ pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/controller/PetAnalysisController.java
@@ -15,9 +15,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,10 +29,10 @@ public class PetAnalysisController {
 
     private final PetAnalysisService petAnalysisService;
 
-    @PostMapping("/")
+    @PostMapping(value = "/", consumes = "multipart/form-data")
     @Operation(summary = "Create new Pet Analysis")
     public ResponseEntity<PetAnalysisResponseDTO> createPetAnalysis(
-        @Valid @RequestBody PetAnalysisRequestDTO petAnalysisRequestDTO,
+        @Valid @ModelAttribute PetAnalysisRequestDTO petAnalysisRequestDTO,
         @AuthenticationPrincipal AuthUserDetails userDetails
     ) {
         return ResponseEntity.status(HttpStatus.CREATED).body(
