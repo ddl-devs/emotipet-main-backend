bd691cc4c0b2de82afa72dc0f0aad5800f117ea0
Lucas <lucasoliveirars22@gmail.com>
2025-02-26 21:15:22 -0300

    :construction: fix: value in UploadImageService

--- pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/service/UploadImageService.java
+++ pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/service/UploadImageService.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class UploadImageService {
 
-    @Value("bucket-name")
+    @Value("${AWS_S3_NAME}")
     private String bucketName;
 
     @Autowired
