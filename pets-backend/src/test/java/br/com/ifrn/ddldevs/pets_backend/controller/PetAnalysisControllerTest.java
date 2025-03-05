c5c3ae41b4b7794cc6b06ca7968f895dd6708f35
Lucas <lucasoliveirars22@gmail.com>
2025-03-05 01:42:02 -0300

    :construction: integration tests recommendations and analysis (to fix)

--- pets-backend/src/test/java/br/com/ifrn/ddldevs/pets_backend/controller/PetAnalysisControllerTest.java
+++ pets-backend/src/test/java/br/com/ifrn/ddldevs/pets_backend/controller/PetAnalysisControllerTest.java
@@ -1,128 +1,230 @@
 package br.com.ifrn.ddldevs.pets_backend.controller;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import br.com.ifrn.ddldevs.pets_backend.SecurityTestConfig;
 import br.com.ifrn.ddldevs.pets_backend.domain.Enums.AnalysisType;
-import br.com.ifrn.ddldevs.pets_backend.dto.keycloak.LoginRequestDTO;
-import br.com.ifrn.ddldevs.pets_backend.keycloak.KeycloakServiceImpl;
+import br.com.ifrn.ddldevs.pets_backend.domain.Enums.Gender;
+import br.com.ifrn.ddldevs.pets_backend.domain.Enums.Species;
+import br.com.ifrn.ddldevs.pets_backend.domain.Pet;
+import br.com.ifrn.ddldevs.pets_backend.domain.PetAnalysis;
+import br.com.ifrn.ddldevs.pets_backend.domain.User;
+import br.com.ifrn.ddldevs.pets_backend.dto.Pet.PetRequestDTO;
+import br.com.ifrn.ddldevs.pets_backend.dto.PetAnalysis.PetAnalysisRequestDTO;
+import br.com.ifrn.ddldevs.pets_backend.dto.User.UserRequestDTO;
+import br.com.ifrn.ddldevs.pets_backend.mapper.PetAnalysisMapper;
+import br.com.ifrn.ddldevs.pets_backend.mapper.PetMapper;
+import br.com.ifrn.ddldevs.pets_backend.mapper.UserMapper;
+import br.com.ifrn.ddldevs.pets_backend.repository.PetAnalysisRepository;
+import br.com.ifrn.ddldevs.pets_backend.repository.PetRepository;
+import br.com.ifrn.ddldevs.pets_backend.repository.UserRepository;
 import br.com.ifrn.ddldevs.pets_backend.service.PetAnalysisService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {
+    "AWS_BUCKET_NAME=emotipet-bucket"
+})
 @ActiveProfiles("test")
+@Import({SecurityTestConfig.class})
 public class PetAnalysisControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
-    private String accessToken;
-    private ObjectMapper objectMapper;
 
     @Mock
     private PetAnalysisService petAnalysisService;
 
-    @InjectMocks
-    private PetAnalysisController petAnalysisController;
+    @Autowired
+    private UserRepository userRepository;
 
-    @Mock
-    private KeycloakServiceImpl keycloakServiceImpl;
+    @Autowired
+    private UserMapper userMapper;
 
-    @InjectMocks
-    private AuthController authController;
+    @Autowired
+    private PetMapper petMapper;
+
+    @Autowired
+    private PetAnalysisMapper petAnalysisMapper;
+
+    @Autowired
+    private PetRepository petRepository;
+
+    @Autowired
+    private PetAnalysisRepository petAnalysisRepository;
+
+    @Autowired
+    private JwtDecoder jwtDecoder;
+
+    @Autowired
+    private TokenUtils tokenUtils;
 
     @MockitoBean
     MockMultipartFile mockImage = new MockMultipartFile(
-        "picture",
-        "pet-analysis-picture.jpg",
+        "file",
+        "user-picture.jpg",
         "image/jpeg",
         "fake-image-content".getBytes()
     );
 
+    private User user;
+
+    private Pet pet;
+
+    private PetAnalysis petAnalysis;
+
     @BeforeEach
     void setUp() throws Exception {
-        this.objectMapper = new ObjectMapper();
-        this.mockMvc = MockMvcBuilders.standaloneSetup(petAnalysisController, authController)
-            .build();
+        MockitoAnnotations.openMocks(this);
 
-        LoginRequestDTO loginRequest = new LoginRequestDTO("admin", "admin", "pets-backend",
-            "password");
+        UserRequestDTO userRequest = new UserRequestDTO(
+            "bell",
+            "belll@email.com",
+            "Bell",
+            "Doe",
+            LocalDate.of(1990, 1, 15),
+            mockImage,
+            "user!123"
+        );
 
-        when(keycloakServiceImpl.generateToken(any(LoginRequestDTO.class)))
-            .thenReturn("mocked-jwt-token");
+        user = userMapper.toEntity(userRequest);
+        user.setKeycloakId(UUID.randomUUID().toString());
+        user = userRepository.save(user);
 
-        MvcResult result = mockMvc.perform(post("/auth/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(loginRequest)))
-            .andExpect(status().isOk())
-            .andReturn();
+        PetRequestDTO petRequest = new PetRequestDTO(
+            "Apolo",
+            Gender.MALE,
+            LocalDate.of(2020, 1, 15),
+            BigDecimal.valueOf(10.0),
+            Species.DOG,
+            "Labrador",
+            30,
+            mockImage
+        );
 
-        this.accessToken = result.getResponse().getContentAsString();
+        pet = petMapper.toEntity(petRequest);
+        pet.setUser(user);
+        pet = petRepository.save(pet);
+
+        System.out.println("petId: " + pet.getId());
+
+        PetAnalysisRequestDTO petAnalysisRequest = new PetAnalysisRequestDTO(
+            pet.getId(),
+            mockImage,
+            AnalysisType.BREED
+        );
+
+        petAnalysis = petAnalysisMapper.toEntity(petAnalysisRequest);
+        petAnalysis = petAnalysisRepository.save(petAnalysis);
+    }
+
+    @AfterEach
+    @Transactional
+    public void tearDown() {
+        petAnalysisRepository.deleteAll();
+        petRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
-    @DisplayName("Deve listar análises com sucesso quando o usuário é admin")
-    void shouldListAnalysisSuccessfully() throws Exception {
-        mockMvc.perform(get("/pet-analysis/")
-                .header("Authorization", "Bearer " + accessToken)
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk());
+    @DisplayName("Deve listar análises com sucesso")
+    void shouldListPetAnalysisSuccessfully() throws Exception {
+        String tokenString = tokenUtils.getToken(user.getEmail());
+        Jwt jwt = tokenUtils.getJwt(tokenString, user, List.of("admin"));
+        when(jwtDecoder.decode(tokenString)).thenReturn(jwt);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/pet-analysis/")
+                    .header("Authorization", "Bearer " + tokenString)
+            )
+            .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
-//    @Test
-//    @DisplayName("Deve retornar Forbidden quando o usuário não é admin")
-//    void shouldReturnForbiddenWhenUserNotAdmin() throws Exception {
-//        mockMvc.perform(get("/pet-analysis/")
-//                .header("Authorization",
-//                    "Bearer " + "UMTuYS2ef4BEQdT9T91PRCCPGVsjLxHB3Hcp3jdmayZIfKm0PaZEaWgrovA9LKaQ")
-//                .contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isForbidden());
-//    }
+    @Test
+    @DisplayName("Deve retornar Forbidden ao listar análises com token de client")
+    void shouldListPetAnalysisNotSuccessfully() throws Exception {
+        String tokenString = tokenUtils.getToken(user.getEmail());
+        Jwt jwt = tokenUtils.getJwt(tokenString, user, List.of("client"));
+        when(jwtDecoder.decode(tokenString)).thenReturn(jwt);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/pet-analysis/")
+                    .header("Authorization", "Bearer " + tokenString)
+            )
+            .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
 
     @Test
     @DisplayName("Deve criar uma análise com sucesso")
     void shouldCreateAnalysisSuccessfully() throws Exception {
-        mockMvc.perform(multipart("/pet-analysis/")
-                .file(mockImage)
-                .header("Authorization", "Bearer " + accessToken)
-                .param("analysisType", AnalysisType.BREED.toString())
-                .param("petId", "1")
-                .header("Authorization", "Bearer " + accessToken))
-            .andExpect(status().isCreated());
+        petAnalysisRepository.deleteAll();
+
+        String tokenString = tokenUtils.getToken(user.getEmail());
+        Jwt jwt = tokenUtils.getJwt(tokenString, user, List.of("client"));
+        when(jwtDecoder.decode(tokenString)).thenReturn(jwt);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.multipart("/pet-analysis/")
+                    .file(mockImage)
+                    .header("Authorization", "Bearer " + tokenString)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .param("analysisType", AnalysisType.BREED.toString())
+                    .param("petId", "1")
+            )
+            .andExpect(MockMvcResultMatchers.status()
+                .isOk());
     }
 
     @Test
     @DisplayName("Deve buscar uma análise com sucesso")
     void shouldGetAnalysisSuccessfully() throws Exception {
-        Long analysisId = 1L;
-        mockMvc.perform(get("/pet-analysis/" + analysisId)
-                .header("Authorization", "Bearer " + accessToken))
+        String tokenString = tokenUtils.getToken(user.getEmail());
+        Jwt jwt = tokenUtils.getJwt(tokenString, user, List.of("client"));
+        when(jwtDecoder.decode(tokenString)).thenReturn(jwt);
+
+        mockMvc.perform(get("/pet-analysis/{id}", petAnalysis.getId())
+                .header("Authorization", "Bearer " + tokenString))
             .andExpect(status().isOk());
     }
 
     @Test
     @DisplayName("Deve deletar uma análise com sucesso")
     void shouldDeleteAnalysisSuccessfully() throws Exception {
-        Long analysisId = 1L;
-        mockMvc.perform(delete("/pet-analysis/" + analysisId)
-                .header("Authorization", "Bearer " + accessToken))
+        String tokenString = tokenUtils.getToken(user.getEmail());
+        Jwt jwt = tokenUtils.getJwt(tokenString, user, List.of("client"));
+        when(jwtDecoder.decode(tokenString)).thenReturn(jwt);
+
+        Long recommendationId = 1L;
+        mockMvc.perform(delete("/pet-analysis/{id}" + petAnalysis.getId())
+                .header("Authorization", "Bearer " + tokenString))
             .andExpect(status().isNoContent());
     }
 }
--- pets-backend/src/test/java/br/com/ifrn/ddldevs/pets_backend/controller/RecommendationControllerTest.java
+++ pets-backend/src/test/java/br/com/ifrn/ddldevs/pets_backend/controller/RecommendationControllerTest.java
@@ -1,110 +1,231 @@
 package br.com.ifrn.ddldevs.pets_backend.controller;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import br.com.ifrn.ddldevs.pets_backend.SecurityTestConfig;
+import br.com.ifrn.ddldevs.pets_backend.domain.Enums.Gender;
 import br.com.ifrn.ddldevs.pets_backend.domain.Enums.RecommendationCategories;
+import br.com.ifrn.ddldevs.pets_backend.domain.Enums.Species;
+import br.com.ifrn.ddldevs.pets_backend.domain.Pet;
+import br.com.ifrn.ddldevs.pets_backend.domain.Recommendation;
+import br.com.ifrn.ddldevs.pets_backend.domain.User;
+import br.com.ifrn.ddldevs.pets_backend.dto.Pet.PetRequestDTO;
 import br.com.ifrn.ddldevs.pets_backend.dto.Recommendation.RecommendationRequestDTO;
-import br.com.ifrn.ddldevs.pets_backend.dto.keycloak.LoginRequestDTO;
+import br.com.ifrn.ddldevs.pets_backend.dto.User.UserRequestDTO;
 import br.com.ifrn.ddldevs.pets_backend.keycloak.KeycloakServiceImpl;
-import br.com.ifrn.ddldevs.pets_backend.service.RecommendationService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import br.com.ifrn.ddldevs.pets_backend.mapper.PetMapper;
+import br.com.ifrn.ddldevs.pets_backend.mapper.RecommendationMapper;
+import br.com.ifrn.ddldevs.pets_backend.mapper.UserMapper;
+import br.com.ifrn.ddldevs.pets_backend.microservice.RecommendationRequestsService;
+import br.com.ifrn.ddldevs.pets_backend.repository.PetRepository;
+import br.com.ifrn.ddldevs.pets_backend.repository.RecommendationRepository;
+import br.com.ifrn.ddldevs.pets_backend.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {
+    "AWS_BUCKET_NAME=emotipet-bucket"
+})
 @ActiveProfiles("test")
+@Import({SecurityTestConfig.class})
 public class RecommendationControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
-    private String accessToken;
-    private ObjectMapper objectMapper;
 
-    @Mock
-    private RecommendationService recommendationService;
-
-    @InjectMocks
-    private RecommendationController recommendationController;
-
-    @Mock
+    @MockitoBean
     private KeycloakServiceImpl keycloakServiceImpl;
 
-    @InjectMocks
-    private AuthController authController;
+    @MockitoBean
+    private RecommendationRequestsService recommendationRequestsService;
+
+    @Autowired
+    private RecommendationRepository recommendationRepository;
+
+    @Autowired
+    private RecommendationMapper recommendationMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private PetMapper petMapper;
+
+    @Autowired
+    private PetRepository petRepository;
+
+    @Autowired
+    private JwtDecoder jwtDecoder;
+
+    @Autowired
+    private TokenUtils tokenUtils;
+
+    @MockitoBean
+    MockMultipartFile mockImage = new MockMultipartFile(
+        "file",
+        "user-picture.jpg",
+        "image/jpeg",
+        "fake-image-content".getBytes()
+    );
+
+    private User user;
+
+    private Pet pet;
+
+    private Recommendation recommendation;
 
     @BeforeEach
     void setUp() throws Exception {
-        this.objectMapper = new ObjectMapper();
-        this.mockMvc = MockMvcBuilders.standaloneSetup(recommendationController, authController)
-            .build();
+        MockitoAnnotations.openMocks(this);
 
-        LoginRequestDTO loginRequest = new LoginRequestDTO("admin", "admin", "pets-backend",
-            "password");
+        UserRequestDTO userRequest = new UserRequestDTO(
+            "alex",
+            "alex@email.com",
+            "Alex",
+            "Doe",
+            LocalDate.of(1990, 1, 15),
+            mockImage,
+            "user!123"
+        );
 
-        when(keycloakServiceImpl.generateToken(any(LoginRequestDTO.class)))
-            .thenReturn("mocked-jwt-token");
+        user = userMapper.toEntity(userRequest);
+        user.setKeycloakId(UUID.randomUUID().toString());
+        user = userRepository.save(user);
 
-        MvcResult result = mockMvc.perform(post("/auth/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(loginRequest)))
-            .andExpect(status().isOk())
-            .andReturn();
+        PetRequestDTO petRequest = new PetRequestDTO(
+            "Apolo",
+            Gender.MALE,
+            LocalDate.of(2020, 1, 15),
+            BigDecimal.valueOf(10.0),
+            Species.DOG,
+            "Labrador",
+            30,
+            mockImage
+        );
 
-        this.accessToken = result.getResponse().getContentAsString();
+        pet = petMapper.toEntity(petRequest);
+        pet.setUser(user);
+        pet = petRepository.save(pet);
+        System.out.println("pet.getId()" + pet.getId());
+
+        RecommendationRequestDTO recommendationRequest = new RecommendationRequestDTO(
+            pet.getId(),
+            RecommendationCategories.HEALTH
+        );
+        recommendation = recommendationMapper.toEntity(recommendationRequest);
+        recommendation = recommendationRepository.save(recommendation);
+    }
+
+    @AfterEach
+    @Transactional
+    public void tearDown() {
+        recommendationRepository.deleteAll();
+        petRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
     @DisplayName("Deve listar recomendações com sucesso")
     void shouldListRecommendationsSuccessfully() throws Exception {
-        mockMvc.perform(get("/recommendations/")
-                .header("Authorization", "Bearer " + accessToken)
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk());
+        String tokenString = tokenUtils.getToken(user.getEmail());
+        Jwt jwt = tokenUtils.getJwt(tokenString, user, List.of("admin"));
+        when(jwtDecoder.decode(tokenString)).thenReturn(jwt);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/recommendations/")
+                    .header("Authorization", "Bearer " + tokenString)
+            )
+            .andExpect(MockMvcResultMatchers.status().isOk());
     }
+
+    @Test
+    @DisplayName("Deve retornar Forbidden ao listar recomendações com token de client")
+    void shouldListRecommendationsNotSuccessfully() throws Exception {
+        String tokenString = tokenUtils.getToken(user.getEmail());
+        Jwt jwt = tokenUtils.getJwt(tokenString, user, List.of("client"));
+        when(jwtDecoder.decode(tokenString)).thenReturn(jwt);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/recommendations/")
+                    .header("Authorization", "Bearer " + tokenString)
+            )
+            .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
 
     @Test
     @DisplayName("Deve criar uma recomendação com sucesso")
     void shouldCreateRecommendationSuccessfully() throws Exception {
-        RecommendationRequestDTO requestDTO = new RecommendationRequestDTO(1L,
-            RecommendationCategories.HEALTH);
+        recommendationRepository.deleteAll();
 
-        mockMvc.perform(post("/recommendations/")
-                .header("Authorization", "Bearer " + accessToken)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(requestDTO)))
-            .andExpect(status().isCreated());
+        String tokenString = tokenUtils.getToken(user.getEmail());
+        Jwt jwt = tokenUtils.getJwt(tokenString, user, List.of("client"));
+        when(jwtDecoder.decode(tokenString)).thenReturn(jwt);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.multipart("/recommendations/")
+                    .header("Authorization", "Bearer " + tokenString)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .param("petId", pet.getId().toString())
+                    .param("categoryRecommendation", RecommendationCategories.HEALTH.toString())
+            )
+            .andExpect(MockMvcResultMatchers.status()
+                .isOk());
     }
 
     @Test
     @DisplayName("Deve buscar uma recomendação com sucesso")
     void shouldGetRecommendationSuccessfully() throws Exception {
-        Long recommendationId = 1L;
-        mockMvc.perform(get("/recommendations/" + recommendationId)
-                .header("Authorization", "Bearer " + accessToken))
+        String tokenString = tokenUtils.getToken(user.getEmail());
+        Jwt jwt = tokenUtils.getJwt(tokenString, user, List.of("client"));
+        when(jwtDecoder.decode(tokenString)).thenReturn(jwt);
+
+        mockMvc.perform(get("/recommendations/{id}", recommendation.getId())
+                .header("Authorization", "Bearer " + tokenString))
             .andExpect(status().isOk());
     }
 
     @Test
     @DisplayName("Deve deletar uma recomendação com sucesso")
     void shouldDeleteRecommendationSuccessfully() throws Exception {
+        String tokenString = tokenUtils.getToken(user.getEmail());
+        Jwt jwt = tokenUtils.getJwt(tokenString, user, List.of("client"));
+        when(jwtDecoder.decode(tokenString)).thenReturn(jwt);
+
         Long recommendationId = 1L;
-        mockMvc.perform(delete("/recommendations/" + recommendationId)
-                .header("Authorization", "Bearer " + accessToken))
+        mockMvc.perform(delete("/recommendations/{id}" + recommendation.getId())
+                .header("Authorization", "Bearer " + tokenString))
             .andExpect(status().isNoContent());
     }
 }
