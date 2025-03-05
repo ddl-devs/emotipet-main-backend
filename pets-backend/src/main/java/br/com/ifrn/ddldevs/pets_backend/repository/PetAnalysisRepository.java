4ee7f4055da7b232498fcde5a5a109a423f3891a
DamiaoFilho <damiaofilho0123@gmail.com>
2025-03-05 02:19:07 -0300

    filters on pet analysis

--- pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/repository/PetAnalysisRepository.java
+++ pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/repository/PetAnalysisRepository.java
@@ -1,8 +1,10 @@
 package br.com.ifrn.ddldevs.pets_backend.repository;
 
 import br.com.ifrn.ddldevs.pets_backend.domain.PetAnalysis;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,8 +12,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface PetAnalysisRepository extends JpaRepository<PetAnalysis, Long> {
-    List<PetAnalysis> findAllByPetId(Long petId);
+public interface PetAnalysisRepository extends JpaRepository<PetAnalysis, Long>, JpaSpecificationExecutor<PetAnalysis>{
+    Page<PetAnalysis> findAll(Pageable pageable);
     @Query("""
         SELECT p FROM PetAnalysis p 
         WHERE p.pet.id = :petId 
