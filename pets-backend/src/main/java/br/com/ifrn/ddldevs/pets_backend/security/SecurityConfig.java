689f7b63f7b68d23f9806cba8b9b5a14dd2d1135
Lucas <lucasoliveirars22@gmail.com>
2025-03-05 22:35:36 -0300

    :sparkles: permit access in actuator

--- pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/security/SecurityConfig.java
+++ pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/security/SecurityConfig.java
@@ -7,8 +7,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -20,14 +20,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/auth/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/users/").permitAll()
-                    .requestMatchers(
-                            "/swagger-ui/**",
-                            "/v3/api-docs/**",
-                            "/swagger-ui.html"
-                    ).permitAll()
-                    .anyRequest().authenticated()
+                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/users/").permitAll()
+                .requestMatchers(
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**",
+                    "/swagger-ui.html"
+                ).permitAll()
+                .requestMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
             )
             .oauth2ResourceServer(
                 oauth2 -> oauth2.jwt(
@@ -35,7 +36,7 @@ public class SecurityConfig {
                 )
             );
         http.sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
         http.cors(AbstractHttpConfigurer::disable);
         return http.build();
